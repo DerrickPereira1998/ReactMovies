9259dda520d54b337b92a586cd345ccce3a369d3
DerrickPereira1998 <derrickpereira1998@gmail.com>
2023-01-07 02:54:22 -0300

    banner remake

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,3 +1,17 @@
-import { Banner } from "./Banner";
+import './Banner.css'
+import { SiReact } from 'react-icons/si';
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/Banner.png" alt="O banner principal da página do App"/>
+            <div>
+                <SiReact color='white' size={60}/>
+                <p>React Movies</p>
+            </div>
+        </header>
+    )
+}
 
 export default Banner
